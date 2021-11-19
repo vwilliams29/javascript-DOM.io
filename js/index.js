5bcf7df04596fed0b163e26acb1b10075e8a6bae
vwilliams29 <56303856+vwilliams29@users.noreply.github.com>
2021-11-19 14:01:45 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// var titles = document.getElementsByClassName('title');
+//
+// console.log(Array.isArray(titles));
+// console.log(Array.isArray(Array.from(titles)));
+//
+// Array.from(titles).forEach(function(item){
+//   console.log(item);
+// })
+
+const wmf = document.querySelector('#book-list li:nth-child(2).name');
+// console.log(wmf);
+
+var books = document.querySelector('#book-list li .name');
+console.log(books);
