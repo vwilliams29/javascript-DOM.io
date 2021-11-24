be06ee69951eb1deac1ff99c8696f8d0605aefcb
vwilliams29 <56303856+vwilliams29@users.noreply.github.com>
2021-11-24 10:59:46 -0500

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -87,9 +87,60 @@ addForm.addEventListener('submit', function(e){
   const bookName = document.createElement('span')
   const deleteBtn = document.createElement('span');
 
+  // add content
+  deleteBtn.textContent = 'delete';
+  bookName.textContent = value;
+
+  //add classes
+  bookName.classList.add('name');
+  deleteBtn.classList.add('delete');
+
+
   //append to document
   li.appendChild(bookName);
   li.appendChild(deleteBtn);
+  list.appendChild(li);
 
 
-})
+});
+
+// hide books
+const hideBox = document.querySelector('#hide');
+hideBox.addEventListener('change', function(e){
+  if(hideBox.checked){
+    list.style.display = "none";
+  }else{
+    list.style.display = "initial";
+  }
+});
+
+//filter Books
+const searchBar = document.forms['search-books'].querySelector('input');
+searchBar.addEventListener('keyup', function(e){
+  const term = e.target.value.toLowerCase();
+  const books = list.getElementsByTagName('li');
+  Array.from(books).forEach(function(book){
+    const title = book.firstElementChild.textContent;
+    if(title.toLowerCase().indexOf(term) != -1){
+      book.style.display = 'block';
+    }else{
+      book.style.display = 'none';
+    };
+  });
+});
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', (e) => {
+  if(e.target.tagName == 'LI'){
+    const targetPanel = document.querySelector(e.target.dataset.target);
+    Array.from(panels).forEach((panel) => {
+      if(panel == targetPanel){
+        panel.classList.add('active');
+      }else{
+        panel.classList.remove('active');
+      }
+    });
+  }
+});
